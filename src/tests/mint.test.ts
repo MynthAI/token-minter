6fab2481273f52d598ffae7c10fbe01ab5e3024f
SynthLuvr <131367121+SynthLuvr@users.noreply.github.com>
2023-07-30 11:31:07 +0200

    Implemented test for token minting

--- src/tests/mint.test.ts
+++ src/tests/mint.test.ts
@@ -1,7 +1,36 @@
+import { BlockFrostAPI } from "@blockfrost/blockfrost-js";
 import test from "ava";
 import config from "config";
 import { generateUsername } from "unique-username-generator";
 import { mint } from "../mint";
+
+const randomSleep = async () => {
+  const sleep = (ms: number) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
+  // Generate a random number between 10 and 30 seconds
+  const randomTime = Math.floor(Math.random() * (30000 - 10000 + 1)) + 10000;
+  await sleep(randomTime);
+};
+
+const getAsset = async (
+  blockfrostApiKey: string,
+  policyId: string,
+  name: string
+) => {
+  const blockfrost = new BlockFrostAPI({
+    projectId: blockfrostApiKey,
+  });
+  const assetId = policyId + Buffer.from(name).toString("hex");
+  while (true) {
+    try {
+      return await blockfrost.assetsById(assetId);
+    } catch {
+      await randomSleep();
+    }
+  }
+};
 
 test("successfully mints a token", async (t) => {
   const params = {
@@ -13,7 +42,13 @@ test("successfully mints a token", async (t) => {
     },
   };
 
+  t.timeout(120000);
   const policyId = await mint(params);
-  console.debug(policyId);
-  t.is(typeof policyId, "string");
+
+  const token = await getAsset(
+    params.blockfrostApiKey,
+    policyId,
+    params.token.name
+  );
+  t.is(BigInt(token.quantity), params.token.amount);
 });
