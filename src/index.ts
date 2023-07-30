184756e26048d58ad48fada9b71facd2e6ec1338
SynthLuvr <131367121+SynthLuvr@users.noreply.github.com>
2023-07-30 11:52:42 +0200

    Created script to mint via cli

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Let's mint some token");
+import config from "config";
+import { mint } from "mint";
+
+const run = async () => {
+  const params = {
+    blockfrostApiKey: config.get<string>("blockfrost"),
+    minterSeed: config.get<string>("wallets.minter"),
+    token: {
+      name: config.get<string>("token.name"),
+      amount: config.get<bigint>("token.supply"),
+    },
+  };
+
+  await mint(params);
+};
+
+run();
