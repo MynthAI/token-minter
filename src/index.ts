c3e81fde86a00112ab024f8e2b18b332e68aca91
SynthLuvr <131367121+SynthLuvr@users.noreply.github.com>
2023-07-31 07:16:40 +0200

    Mint tokens using Lucid

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Let's mint some token");
+import config from "config";
+import { mint } from "mint";
+
+const run = async () => {
+  const params = {
+    blockfrostApiKey: config.get<string>("blockfrost"),
+    minterSeed: config.get<string>("wallets.minter"),
+    token: {
+      name: config.get<string>("token.name"),
+      amount: config.get<bigint>("token.supply"),
+    },
+  };
+
+  const shouldMint = process.argv.includes("--mint");
+  await mint(params, !shouldMint);
+};
+
+run();
---
+++ src/mint.ts
@@ -0,0 +1,90 @@
+import fs from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import {
+  Blockfrost,
+  fromText,
+  Lucid,
+  NativeScript,
+  Network,
+} from "lucid-cardano";
+import invariant from "tiny-invariant";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const expiresIn = 200000; // About 3 minutes
+
+const loadLucid = async (seed: string, blockfrostApiKey: string) => {
+  const network = blockfrostApiKey.substring(0, 7);
+  invariant(network);
+  const lucid = await Lucid.new(
+    new Blockfrost(
+      `https://cardano-${network}.blockfrost.io/api/v0`,
+      blockfrostApiKey
+    ),
+    (network.charAt(0).toUpperCase() + network.slice(1)) as Network
+  );
+
+  lucid.selectWalletFromSeed(seed);
+  return lucid;
+};
+
+const getKeyHash = (lucid: Lucid, address: string) => {
+  const { paymentCredential } = lucid.utils.getAddressDetails(address);
+  invariant(paymentCredential);
+  return paymentCredential.hash;
+};
+
+const loadMintingPolicy = () => {
+  const filePath = path.join(__dirname, "..", "minting-policy.json");
+  const rawData = fs.readFileSync(filePath, "utf-8");
+  const jsonData: NativeScript = JSON.parse(rawData);
+
+  return jsonData;
+};
+
+interface Config {
+  blockfrostApiKey: string;
+  minterSeed: string;
+  token: {
+    name: string;
+    amount: bigint;
+  };
+}
+
+const mint = async (config: Config, dryrun: boolean = true) => {
+  console.log("Loading minter wallet");
+  const minter = await loadLucid(config.minterSeed, config.blockfrostApiKey);
+  const [address, utxos] = await Promise.all([
+    minter.wallet.address(),
+    minter.wallet.getUtxos(),
+  ]);
+
+  invariant(utxos.length, `${address} needs to be funded`);
+
+  console.debug("Creating minting policy");
+  const mintingPolicy = loadMintingPolicy();
+  invariant(mintingPolicy.scripts, "Minting policy is invalid");
+  const minterHash = getKeyHash(minter, address);
+  const expiration = minter.utils.unixTimeToSlot(Date.now() + expiresIn);
+  mintingPolicy.scripts[0].slot = expiration;
+  mintingPolicy.scripts[1].keyHash = minterHash;
+  const script = minter.utils.nativeScriptFromJson(mintingPolicy);
+  const policyId = minter.utils.mintingPolicyToId(script);
+  const unit = policyId + fromText(config.token.name);
+
+  console.debug("Building transaction");
+  const tx = await minter
+    .newTx()
+    .mintAssets({ [unit]: config.token.amount })
+    .validTo(Date.now() + expiresIn)
+    .attachMintingPolicy(script)
+    .complete();
+
+  const signedTx = await tx.sign().complete();
+  if (dryrun === false) await signedTx.submit();
+  console.debug("Minted token with policy ID", policyId);
+  return policyId;
+};
+
+export { mint };
