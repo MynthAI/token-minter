bfeb8dcc0e0acd37f523812750ec982ed0ea5e35
SynthLuvr <131367121+SynthLuvr@users.noreply.github.com>
2023-08-10 21:04:31 +0200

    Allow minting of many tokens at once

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,37 @@
 import config from "config";
 import { mint } from "mint";
 
+type Token = {
+  name: string;
+  supply: bigint;
+};
+
+type Tokens = {
+  [ticker: string]: Token;
+};
+
 const run = async () => {
   const params = {
     blockfrostApiKey: config.get<string>("blockfrost"),
     minterSeed: config.get<string>("wallets.minter"),
     ownerKey: config.get<string>("wallets.owner"),
-    token: {
-      name: config.get<string>("token.name"),
-      amount: config.get<bigint>("token.supply"),
-    },
   };
-
+  const tokens = config.get<Tokens>("tokens");
   const shouldMint = process.env.MINT === "true";
-  await mint(params, !shouldMint);
+
+  for (const ticker in tokens) {
+    const token = tokens[ticker];
+    await mint(
+      {
+        ...params,
+        token: {
+          name: token.name,
+          amount: token.supply,
+        },
+      },
+      !shouldMint
+    );
+  }
 };
 
 run();
